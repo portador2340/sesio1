3686befcab2e2543870afaadd8751b3426a358ed
kllisman <89989898+kllisman@users.noreply.github.com>
2022-05-04 12:53:59 -0500

    primer commit

---
+++ app/src/main/java/edu/ilp/lifecycle/MainActivity.kt
@@ -0,0 +1,14 @@
+package edu.ilp.lifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity", "Metodo onCreate")
+    }
+}
